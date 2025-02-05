141e48acd0d4fba9e050f141cb6c4b92b6e0f2e3
Seila-Dev <erickrodrigues3975@gmail.com>
2025-02-05 17:16:04 -0300

    fix: admin header responsive

--- src/components/adminHeader/index.tsx
+++ src/components/adminHeader/index.tsx
@@ -1,11 +1,12 @@
 import styled from "styled-components"
 import imagemParaTeste from "@/assets/canecaalt.png"
+import { Link } from "react-router-dom"
 
 export const Header = () => {
 
     return (
         <HeaderElement>
-            <h1>Admin Painel</h1>
+            <h1><Link to="/admin">Admin Painel</Link></h1>
             <div className="user-profile">
                 <p className="user-name">Erick Rodrigues</p>
                 <img src={imagemParaTeste} alt="user-image" />
@@ -20,15 +21,24 @@ const HeaderElement = styled.header`
     justify-content: space-between;
     padding: 15px 40px;
     max-height: 100%;
+    border-bottom: 1px solid var(--light-purple);
 
     .user-profile{
         display: flex;
         align-items: center;
+        cursor: pointer;
     }
     .user-profile img{
         margin-left: 20px;
         border-radius: 10px;
         width: 40px;
         height: 40px;
+    }
+
+    @media(max-width: 768px){
+        padding: 10px;
+        h1{
+            font-size: 25px;
+        }
     }
 `
